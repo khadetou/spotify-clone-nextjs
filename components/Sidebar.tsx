8ac73121893a6698fd6ae81f7716559786c98e5d
khadetou <khadetou96@gmail.com>
2023-07-08 21:22:34 +0000

    header sidebarItem sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,6 +4,9 @@ import { FC, PropsWithChildren, useMemo } from "react";
 import { usePathname } from "next/navigation";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import Box from "./Box";
+import SidebarItem from "./SidebarItem";
+import Library from "./Library";
 
 const Sidebar: FC<PropsWithChildren> = ({ children }) => {
   const pathname = usePathname();
@@ -26,7 +29,19 @@ const Sidebar: FC<PropsWithChildren> = ({ children }) => {
   );
   return (
     <div className="flex h-full">
-      <div className="hidden mx:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2"></div>
+      <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
+        <Box>
+          <div className="flex flex-col gap-y-4 px-5 py-4">
+            {routes.map((route) => (
+              <SidebarItem key={route.label} {...route} />
+            ))}
+          </div>
+        </Box>
+        <Box className="overflow-y-auto h-full">
+          <Library />
+        </Box>
+      </div>
+      <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
     </div>
   );
 };
